053fb5633da6091f3ca5d8af28ac4b49985a647f
Chris Tram <chris.tram@gmail.com>
2018-07-06 18:40:36 -0700

    test out osmosis -- scrape art pages

--- js/webscraper.js
+++ js/webscraper.js
@@ -1,3 +1,19 @@
 var osmosis = require('osmosis');
 
-console.log(osmosis)
+var url = 'http://sanjoseca.gov/Facilities/Facility/Search';
+var data =
+  'featureIDs=&categoryIDs=15&occupants=null&keywords=&pageSize=500&pageNumber=1&sortBy=3&currentLatitude=null&currentLongitude=null&isReservableOnly=false';
+
+osmosis
+  .post(url, data)
+  .find('h3 > a')
+  .follow('@href')
+  .set({
+    title: '.editorContent .Subhead1',
+    artist: '.Subhead2',
+    href: '@href',
+    everything: '.editorContent'
+  })
+  .data(function(_data) {
+    console.log('the data', _data);
+  });
