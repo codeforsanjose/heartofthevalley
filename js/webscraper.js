18be26ceee66538a64242189b08615385694a494
Chris Tram <chris.tram@gmail.com>
2018-07-06 18:42:21 -0700

    adds comments

--- js/webscraper.js
+++ js/webscraper.js
@@ -1,14 +1,16 @@
 var osmosis = require('osmosis');
 
 var url = 'http://sanjoseca.gov/Facilities/Facility/Search';
+
+// categoryID = 15 is 'public art'
 var data =
   'featureIDs=&categoryIDs=15&occupants=null&keywords=&pageSize=500&pageNumber=1&sortBy=3&currentLatitude=null&currentLongitude=null&isReservableOnly=false';
 
 osmosis
   .post(url, data)
-  .find('h3 > a')
-  .follow('@href')
-  .set({
+  .find('h3 > a') // selector for links to indivdual pages about art work
+  .follow('@href') // follow link to individual page about the art work
+  .set({ // grab the appropriate details about the art work
     title: '.editorContent .Subhead1',
     artist: '.Subhead2',
     href: '@href',
