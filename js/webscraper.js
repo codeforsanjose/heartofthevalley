1e3eb810572f0732d8a9472b54ed0e80b359a929
Chris Tram <chris.tram@gmail.com>
2018-07-07 18:05:18 -0700

    WIP formatting scraped info of artworks

--- js/webscraper.js
+++ js/webscraper.js
@@ -1,21 +1,85 @@
-var osmosis = require('osmosis');
+const osmosis = require('osmosis');
+const fs = require('fs');
+const url = 'http://sanjoseca.gov/Facilities/Facility/Search';
+const artworks = [];
+const pageSize = 1; // number of artwork listings per page
+const categoryIDs = 15; // categoryID = 15 is 'public art'
+const httpBody = `featureIDs=&categoryIDs=${categoryIDs}&occupants=null&keywords=&pageSize=${pageSize}&pageNumber=1&sortBy=3&currentLatitude=null&currentLongitude=null&isReservableOnly=false`;
 
-var url = 'http://sanjoseca.gov/Facilities/Facility/Search';
-
-// categoryID = 15 is 'public art'
-var data =
-  'featureIDs=&categoryIDs=15&occupants=null&keywords=&pageSize=500&pageNumber=1&sortBy=3&currentLatitude=null&currentLongitude=null&isReservableOnly=false';
+const cleanUpRules = {
+  artist: [/artists*:/i, /\d+/],
+  description: [
+    function(item) {
+      const { artist, title } = item;
+      return item.description.replace(artist, '').replace(title, '');
+    },
+    /report a concern/i,
+    '(408) 793-4330',
+    'publicart@sanjoseca.gov',
+    /[\r\n]+/gm,
+    /artists*:/i,
+    /District.*/g
+  ]
+};
 
 osmosis
-  .post(url, data)
+  .post(url, httpBody)
   .find('h3 > a') // selector for links to indivdual pages about art work
   .follow('@href') // follow link to individual page about the art work
-  .set({ // grab the appropriate details about the art work
+  .set({
+    // grab and store the appropriate details about the art work
     title: '.editorContent .Subhead1',
     artist: '.Subhead2',
-    href: '@href',
-    everything: '.editorContent'
+    description: '.editorContent'
   })
   .data(function(_data) {
-    console.log('the data', _data);
+    artworks.push(_data);
+  })
+  .done(function() {
+    const cleanedArtworks = [];
+    for (let artwork of artworks) {
+      artwork = cleanUpItem(artwork);
+      artwork = setYear(artwork);
+      cleanedArtworks.push(artwork);
+    }
+    fs.writeFile('wip_artworks.json', JSON.stringify(cleanedArtworks), { flags: 'r+' }, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+    console.log('all done!');
   });
+
+function cleanUpItem(item) {
+  for (let key in item) {
+    item[key] = cleanText(key, item);
+  }
+  return item;
+}
+
+function setYear(item) {
+  let year = item.description.match(/\d{4}/);
+  if (year) {
+    year = parseInt(year[0]);
+    item.year = year;
+    item.description = item.description.replace(year, '');
+  } else {
+    item.year = null;
+  }
+  return item;
+}
+
+function cleanText(key, item) {
+  if (cleanUpRules[key]) {
+    let newText = item[key];
+    for (let rule of cleanUpRules[key]) {
+      if (typeof rule === 'function') {
+        newText = rule(item);
+        continue;
+      }
+      newText = newText.replace(rule, '');
+    }
+    item[key] = newText;
+  }
+  return item[key].trim();
+}
