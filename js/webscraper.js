5c1a9bb4ac9b02aebc24cd23a71a9a00cb9ccf3a
Chris Tram <chris.tram@gmail.com>
2018-07-08 17:09:33 -0700

    add comments

--- js/webscraper.js
+++ js/webscraper.js
@@ -1,15 +1,17 @@
 const readline = require('readline');
 const PATH_OUTPUT_FILE = 'wip_artworks.json';
 const ArtworkScraper = require('./artwork-scraper');
+
+// Config
 const numArtworksToScrape = 2; // number of artwork listings per page
 const categoryIDs = 15; // categoryID = 15 is category for 'public art'
 const httpBody = `featureIDs=&categoryIDs=${categoryIDs}&occupants=null&keywords=&pageSize=${numArtworksToScrape}&pageNumber=1&sortBy=3&currentLatitude=null&currentLongitude=null&isReservableOnly=false`;
 const HOST = 'http://sanjoseca.gov';
 const URL = `${HOST}/Facilities/Facility/Search`;
+const DEBUG_MODE = false; // Debug mode skips terminal prompt; necessary if running the program through IDE debugger.
 
 (function main() {
   let readlineInterface;
-  const DEBUG_MODE = true; // Debug mode skips terminal prompt; necessary if running the program through IDE debugger.
 
   new Promise((resolve, reject) => {
     if (DEBUG_MODE) {
@@ -39,7 +41,7 @@ const URL = `${HOST}/Facilities/Facility/Search`;
       new ArtworkScraper(PATH_OUTPUT_FILE, HOST, URL, httpBody).scrapeAndWriteData();
     })
     .catch(err => {
-      throw err
+      err ? console.error(err) : null;
       console.log('aborted.');
       if (!DEBUG_MODE) {
         readlineInterface.close();
