076f9b58365e2466f4e9027078081f6e3118ef88
Joseph <jrichardson@jmstudios.net>
2018-08-04 08:59:29 -0700

    remove constants.js file from repo

--- config/constants.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  MAPBOX_API_TOKEN:
-    'pk.eyJ1IjoieWNob3kiLCJhIjoiY2pmOTYwdzZ5MG52dDJ3b2JycXY4ZDU5ciJ9.m9H_Mqu1b42AObg_u_tjpA',
-  PATH_ARTWORKS_LIST: path.resolve(__dirname + '/../artwork-data/artworks.json'),
-  URL_SAN_JOSE_GOV: 'http://sanjoseca.gov'
-};
