674d2f3a69817820689e19cdb026673f9ac5f845
Chris Tram <chris.tram@gmail.com>
2018-08-05 16:49:07 -0700

    ignore scraped artworks file, but not the public artworks file

--- config/constants.js
+++ config/constants.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   MAPBOX_API_TOKEN:
     'pk.eyJ1IjoieWNob3kiLCJhIjoiY2pmOTYwdzZ5MG52dDJ3b2JycXY4ZDU5ciJ9.m9H_Mqu1b42AObg_u_tjpA',
-  PATH_ARTWORKS_LIST: path.resolve(__dirname + '/../artwork-data/artworks.json'),
+  PATH_ARTWORKS_LIST_SCRAPED: path.resolve(__dirname + '/../artwork-data/artworks-scraped.json'),
   PATH_ARTWORKS_LIST_PUBLIC: path.resolve(
     __dirname + '/../artwork-data/artworks-manually-fixed.json'
   ),
