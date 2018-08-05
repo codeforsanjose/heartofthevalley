574e6c7881a9492bb7a0c7c1aae9eb4ce37120b2
Chris Tram <chris.tram@gmail.com>
2018-08-04 21:47:48 -0700

    handles error if artworks file is not found

--- server/app.js
+++ server/app.js
@@ -10,11 +10,14 @@ app.get('/', (_req, res) => {
 });
 
 app.get('/api/artworks', (_req, res) => {
-  res.sendFile(CONSTANTS.PATH_ARTWORKS_LIST);
+  res.sendFile(CONSTANTS.PATH_ARTWORKS_LIST, {}, err => {
+    console.error(err);
+    console.error('something wrong happened while trying to find artworks file');
+  });
 });
 
-app.get('/api/mapboxkeys', (req, res) => {
-  res.send({'mapbox_token': CONSTANTS.MAPBOX_API_TOKEN});
+app.get('/api/mapboxkeys', (_req, res) => {
+  res.send({ mapbox_token: CONSTANTS.MAPBOX_API_TOKEN });
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
