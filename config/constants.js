486854d37695aa0f966365edd14e6924a61d818f
Chris Tram <chris.tram@gmail.com>
2018-08-06 01:25:13 -0700

    adds path for errors file

--- config/constants.js
+++ config/constants.js
@@ -3,9 +3,8 @@ const path = require('path');
 module.exports = {
   MAPBOX_API_TOKEN:
     'pk.eyJ1IjoieWNob3kiLCJhIjoiY2pmOTYwdzZ5MG52dDJ3b2JycXY4ZDU5ciJ9.m9H_Mqu1b42AObg_u_tjpA',
-  PATH_ARTWORKS_LIST_SCRAPED: path.resolve(__dirname + '/../artwork-data/artworks-scraped.json'),
-  PATH_ARTWORKS_LIST_PUBLIC: path.resolve(
-    __dirname + '/../artwork-data/artworks-manually-fixed.json'
-  ),
+  PATH_ARTWORKS_SCRAPED: path.resolve(__dirname + '/../artwork-data/artworks-scraped.json'),
+  PATH_ARTWORKS_PUBLIC: path.resolve(__dirname + '/../artwork-data/artworks-manually-fixed.json'),
+  PATH_ARTWORKS_ERRORS: path.resolve(__dirname + '/../artwork-data/artworks-errors.json'),
   URL_SAN_JOSE_GOV: 'http://sanjoseca.gov'
 };
