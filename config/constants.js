f8ac9d4537f2272143c958059a960bb52d5bb884
Chris Tram <chris.tram@gmail.com>
2018-08-07 23:24:10 -0700

    revise JSON file names

--- config/constants.js
+++ config/constants.js
@@ -1,13 +1,18 @@
 const path = require('path');
 
 const URL_SAN_JOSE_GOV = 'http://sanjoseca.gov';
+const PATH_CONSOLIDATED_ARTWORKS = path.resolve(
+  __dirname + '/../artwork-data/consolidated-artworks.json'
+);
 
 module.exports = {
   MAPBOX_API_TOKEN:
     'pk.eyJ1IjoieWNob3kiLCJhIjoiY2pmOTYwdzZ5MG52dDJ3b2JycXY4ZDU5ciJ9.m9H_Mqu1b42AObg_u_tjpA',
-  PATH_ARTWORKS_SCRAPED: path.resolve(__dirname + '/../artwork-data/artworks-scraped.json'),
-  PATH_ARTWORKS_PUBLIC: path.resolve(__dirname + '/../artwork-data/artworks-scraped.json'),
-  PATH_ARTWORKS_ERRORS: path.resolve(__dirname + '/../artwork-data/artworks-errors.json'),
+  PATH_CONSOLIDATED_ARTWORKS,
+  PATH_ARTWORKS_PUBLIC: PATH_CONSOLIDATED_ARTWORKS,
+  PATH_SCRAPED_ARTWORKS_ERRORS: path.resolve(
+    __dirname + '/../artwork-data/scraped-artworks-errors.json'
+  ),
   PATH_ARTWORKS_OVERRIDES: path.resolve(__dirname + '/../artwork-data/artworks-overrides.json'),
   URL_SAN_JOSE_GOV,
   URL_SEARCH: `${URL_SAN_JOSE_GOV}/Facilities/Facility/Search`
--- scripts/scrape-artworks.js
+++ scripts/scrape-artworks.js
@@ -3,10 +3,10 @@ const CONSTANTS = require('../config/constants');
 const ArtworkScraper = require('../lib/artwork-scraper');
 
 // number of artwork listings per page, as of 7/16/18, there are 128 artwork pages at http://sanjoseca.gov/Facilities
-const NUM_ARTWORKS_TO_SCRAPE = 20;
-const MERGE_OVERRIDES = true;
+const NUM_ARTWORKS_TO_SCRAPE = 300;
+const MERGE_OVERRIDES = false;
 // Debug mode skips terminal prompt; necessary if running the program through IDE .
-const DEBUG_MODE = true;
+const DEBUG_MODE = false;
 
 (function main() {
   let readlineInterface;
@@ -23,15 +23,15 @@ const DEBUG_MODE = true;
     });
 
     readlineInterface.question(
-      `This action will overwrite the two files \n\n${CONSTANTS.PATH_ARTWORKS_SCRAPED} \n${
-        CONSTANTS.PATH_ARTWORKS_ERRORS
+      `This action will overwrite the two files \n\n${CONSTANTS.PATH_CONSOLIDATED_ARTWORKS} \n${
+        CONSTANTS.PATH_SCRAPED_ARTWORKS_ERRORS
       } \n\nif it exists. Are you sure you want to continue?\n`,
       answer => {
         answer = answer.toLowerCase();
         if (answer === 'yes' || answer === 'y') {
           return resolve();
         }
-        reject();
+        reject('Aborted.');
       }
     );
   })
@@ -42,7 +42,6 @@ const DEBUG_MODE = true;
       }).run();
     })
     .catch(err => {
-      console.error('Aborted.');
       console.error(err);
     })
     .then(() => {
