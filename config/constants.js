47cbf20b51b1be70c42025f1af2ccb45f0770f59
Chris Tram <chris.tram@gmail.com>
2018-08-11 14:35:17 -0700

    adds local URL to constants

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,8 @@
 const URL_SAN_JOSE_GOV = 'http://sanjoseca.gov';
+const path = require('path');
 
 module.exports = {
   URL_SAN_JOSE_GOV,
-  URL_SEARCH: `${URL_SAN_JOSE_GOV}/Facilities/Facility/Search`
+  URL_SEARCH: `${URL_SAN_JOSE_GOV}/Facilities/Facility/Search`,
+  URL_INDEX_HTML: path.resolve(__dirname + '/../public/index.html')
 };
