eaa62f5755adcebc65cd6bcab5b97d412142d6f1
Gabriel <gab.atienza@gmail.com>
2019-12-17 17:16:58 -0800

    changed url, cleaned artwork properties

--- config/constants.js
+++ config/constants.js
@@ -2,5 +2,5 @@ const URL_SAN_JOSE_GOV = 'https://sanjoseca.gov';
 
 module.exports = {
   URL_SAN_JOSE_GOV,
-  URL_SEARCH: `${URL_SAN_JOSE_GOV}/Facilities/Facility/Search`
+  URL_SEARCH: `${URL_SAN_JOSE_GOV}/your-government/departments/office-of-cultural-affairs/public-art/collections`,
 };
