764db9a9a800de2b449f111795cdecd5e1d64741
Gabriel <gab.atienza@gmail.com>
2020-01-22 14:10:11 -0800

    finish details for edit page

--- scripts/check-POIs.js
+++ scripts/check-POIs.js
@@ -3,6 +3,10 @@ const app = express();
 const { PATH_SCRAPED_ARTWORKS, PATH_CHECKED_POIs } = require('../config/file-paths');
 const Artwork = require('../lib/artwork.js');
 const fs = require('fs');
+const path = require('path');
+const { getGeolocationForArtwork } = require('../lib/geocoding-service.js');
+
+
 
 const PORT = 3000;
 const scrapedArtworks = require(PATH_SCRAPED_ARTWORKS);
@@ -11,7 +15,7 @@ const scrapedArtworksObj = {};
 
 scrapedArtworks.forEach(artwork => {
   scrapedArtworksObj[artwork.id] = artwork;
-})
+});
 
 app.get('/', (req, res) => 
   res.render('../debug_views/index.ejs', {scrapedArtworks, checkedPOIs})
@@ -20,9 +24,13 @@ app.get('/', (req, res) =>
 app.get('/POI/:id', (req, res) => 
   res.render('../debug_views/show.ejs', {artwork: 
     //scrapedArtworksObj[req.params.id]
-    checkedPOIs[0]
+    checkedPOIs[0], getGeolocationForArtwork
   })
 );
+
+// serve static files
+app.use('/modules', express.static(path.join(__dirname, '/../node_modules')));
+app.use('/static', express.static(path.join(__dirname, '/../lib')));
 
 
 app.listen(PORT, () => {
