d7bbc4f6ef75ed4d2534d01c7a9c9e54a5568f09
nick <nick.kats@gmail.com>
2024-02-23 12:51:09 -0800

    fixed a spacing issue

--- app/client/src/utils/API_URL.js
+++ app/client/src/utils/API_URL.js
@@ -1,12 +1,12 @@
 function getApiUrl() {
-    const host = process.env.REACT_APP_API_SERVER;
-    const subDir = process.env.API_BASE_URL;
-    const url = new URL(subDir, host).toString();
-    if (url.endsWith('/')) {
-        return url.substring(0, url.length - 1);
-    }
-    return url;
+  const host = process.env.REACT_APP_API_SERVER;
+  const subDir = process.env.API_BASE_URL;
+  const url = new URL(subDir, host).toString();
+  if (url.endsWith('/')) {
+    return url.substring(0, url.length - 1);
+  }
+  return url;
 }
 module.exports = {
-    API_URL: getApiUrl(),
+  API_URL: getApiUrl(),
 };
