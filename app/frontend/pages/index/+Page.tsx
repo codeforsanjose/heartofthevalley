cdadb2297efd3b2d23d2108a9ea6b0a2407a8fde
simone-lofaso <99053610+simone-lofaso@users.noreply.github.com>
2025-08-11 12:31:53 -0700

    homepage and search bar

--- app/frontend/pages/index/+Page.tsx
+++ app/frontend/pages/index/+Page.tsx
@@ -1,16 +1,15 @@
 import { Counter } from "./Counter.js";
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 
 export default function Page() {
   return (
-    <>
-      <h1 className={"font-bold text-3xl pb-4"}>My Vike app</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
+    <main>
+      <h1 className={"font-bold text-3xl pb-4"}>Explore Art in the Bay Area</h1>
+      <form className="relative w-lg">
+        <input placeholder="Search by art title or zipcode" className="flex-grow px-4 py-2 rounded-full bg-gray-100 focus:outline-none text-gray-700 w-full" />
+        <button type="submit" className="bg-[#164E63] p-3 rounded-full flex items-center justify-center ml-2 absolute right-0.5 top-0"><MagnifyingGlassIcon className="size-4 text-white" /></button>
+
+      </form>
+    </main>
   );
 }
